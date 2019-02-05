102a1f502419c28531ff06570127729ab36407d9
pc <rafaelgoulartb@gmail.com>
2019-02-05 06:51:07 -0200

    Registrando novos produtos

--- api/controllers/produtos.js
+++ api/controllers/produtos.js
@@ -1,34 +1,31 @@
 module.exports = app => {
-  app.get('/product', (req, res) => {
-    let connection = app.dao.connectionFactory();
-    let productsDAO = new app.dao.productsDAO(connection);
 
+  app.get('/product', (req, res) => {
+    const connection = app.dao.connectionFactory();
+    const productsDAO = new app.dao.productsDAO(connection);
     productsDAO.listAll()
       .then(result => res.send(result))
       .catch(err => console.log)
-  })
+  });
+
   app.get('/product/:id/', (req, res) => {
+    const connection = app.dao.connectionFactory();
+    const productsDAO = new app.dao.productsDAO(connection);
     const id = req.params.id;
-    let connection = app.dao.connectionFactory();
-    let productsDAO = new app.dao.productsDAO(connection);
 
     productsDAO.get(id)
       .then(result => res.send(result))
       .catch(console.log)
   });
+
   app.post('/product', (req, res) => {
+    const connection = app.dao.connectionFactory();
+    const productsDAO = new app.dao.productsDAO(connection);
     const product = req.body;
-    res.send(product);
-    console.log(product);
-    
-
-  //   let connection = app.dao.connectionFactory();
-  //   let productsDAO = new app.dao.productsDAO(connection);
-
-  //   productsDAO.add(product)
-  //     .then(result => res.send(result))
-  //     .catch(error => res.send(error))
-  })
+    productsDAO.add(product)
+      .then(result => res.send(result))
+      .catch(error => res.send(error))
+  });
 
 
 };
--- api/dao/productsDAO.js
+++ api/dao/productsDAO.js
@@ -22,7 +22,7 @@ class ProductsDAO {
   }
   add(product) {
     return new Promise((resolve, reject) => {
-      this.connection.query('insert into products (name, description, category, price, img_url) VALUES ?', product, (error, result) => {
+      this.connection.query('insert into products set ?', product, (error, result) => {
         if(error) return reject(error);
         return resolve(result);
       });
