69066252201bc205200332e06f67ce7993e1a2e2
RafaelGoulartB <rafaelgoulartb@gmail.com>
2019-03-17 18:06:18 -0300

    Deleting Old project and start a new

--- app/index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const app = require('./config/custom-express');
-
-const {PORT} = process.env;
-
-app.listen(PORT, () => console.log(`Server is on at http://localhost:${PORT}`));
--- app/routes/cart.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  // Items in Car
-  app.get('/cart', (req, res) => {
-
-  });
-  app.post('/cart', (req, res) => {
-
-  });
-};
