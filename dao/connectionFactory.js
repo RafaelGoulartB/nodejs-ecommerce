e338b2d4d81cf56af7c7498495046eccd2261b3d
RafaelGoulartB <rafaelgoulartb@gmail.com>
2019-03-25 17:12:23 -0300

    ConnectionFactory & Mysql & Eslint

---
+++ dao/connectionFactory.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+function connectionFactory() {
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'equantom'
+  });
+}
+
+module.exports = () => connectionFactory;
