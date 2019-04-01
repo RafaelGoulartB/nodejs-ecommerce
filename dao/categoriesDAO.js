650ae4546130bb3bd65547f8548ab5aff4a6f76e
RafaelGoulartB <rafaelgoulartb@gmail.com>
2019-04-01 17:41:51 -0300

    Nav top & bottom desktop

---
+++ dao/categoriesDAO.js
@@ -0,0 +1,25 @@
+class categoriesDAO {
+  constructor(connection) {
+    this.connection = connection;
+  }
+  list(limit=null) {
+    return new Promise((resolve, reject) => {
+      if(limit) {
+        this.connection
+          .query('select * from product_categories limit ?', limit,
+            (err, result) => {
+              if(err) return reject(err);
+              return resolve(result);
+            });
+      }
+      this.connection
+        .query('select * from product_categories',
+          (err, result) => {
+            if(err) return reject(err);
+            return resolve(result);
+          })
+    });
+  }
+}
+
+module.exports = () => categoriesDAO;
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,20 @@
 module.exports = (app) => {
   app.get('/', (req, res) => {
     let success, warning = app.helpers.msg(req);
-    res.status(200).render('home/index', {
-      title: 'Home',
-      success: 'Opa, essa é uma mensagem de suceso', warning: 'Essa é uma mesagem de erro',
-    });
+    let categories;
+    const connection = app.dao.connectionFactory();
+    const categoriesDAO = new app.dao.categoriesDAO(connection);
+    categoriesDAO.list()
+      .then(result => categories = result)
+      .catch(err => warning = err);
+
+    setTimeout(() => {
+      res.status(200).render('home/index', {
+        title: 'Home',
+        categories,
+        success, warning,
+      });
+    }, 80)
+
   })
 }
